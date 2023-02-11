9602e78b2b09bd57d7b3150ce1b22b83c819aeca
Angelo  Menti <angelo.omarzinho@outlook.com>
2023-02-10 23:08:16 -0300

    fix - verifyToken idUser

--- src/modules/auth/useCases/verifyToken/verifyTokenUseCase.ts
+++ src/modules/auth/useCases/verifyToken/verifyTokenUseCase.ts
@@ -2,28 +2,27 @@ import { AppError } from "../../../../errors/appError";
 import * as jwt from "jsonwebtoken";
 import { JwtVerify, VerifyTokenDTO } from "../../types/verifyToken";
 import { prisma } from "../../../../config/prismaClient";
+import crypto from "crypto-js";
 require("dotenv").config();
 
 export class VerifyTokenUseCase {
   async execute({ refreshToken, accessToken }: VerifyTokenDTO) {
     let newRefreshToken = ''
     let newAccessToken = ''
-    let idUser = ''
     let hasError = false
     
     try {
-      await prisma.user.findFirstOrThrow({where: { refreshToken: refreshToken, expiresAt: { gte: new Date() } }})
+      const idUser = (await prisma.user.findFirstOrThrow({where: { refreshToken: refreshToken, expiresAt: { gte: new Date() } }})).id
 
       jwt.verify(accessToken, process.env.SECRET, async (err, decoded: JwtVerify) => {
         if (err?.name  == 'TokenExpiredError'){
-          newAccessToken = jwt.sign({ id: decoded.id }, process.env.SECRET, { expiresIn: '1h' });
-          newRefreshToken = CryptoJS.HmacSHA1(newAccessToken, process.env.SECRET).toString()
-          idUser = decoded.id
+          newAccessToken = jwt.sign({ id: idUser }, process.env.SECRET, { expiresIn: '1h' });
+          newRefreshToken = crypto.HmacSHA1(newAccessToken, process.env.SECRET).toString()
 
           let expiresAt = new Date()
           expiresAt.setDate(new Date().getDate() + 7)
 
-          await prisma.user.update({where: { id: decoded.id }, data: { refreshToken: newRefreshToken, expiresAt } })
+          await prisma.user.update({where: { id: idUser }, data: { refreshToken: newRefreshToken, expiresAt } })
         }
 
         else if(err) hasError = true
@@ -31,7 +30,6 @@ export class VerifyTokenUseCase {
         else {
           newAccessToken = accessToken
           newRefreshToken = refreshToken
-          idUser = decoded.id
         }
       })
 
