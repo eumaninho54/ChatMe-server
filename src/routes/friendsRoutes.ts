a3337a0930923779863854b2b7ecb99a39bb9215
Angelo  Menti <angelo.omarzinho@outlook.com>
2023-03-04 23:28:31 -0300

    routes - friends routes

--- src/routes/friendsRoutes.ts
+++ src/routes/friendsRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { verifyTokenController } from "../modules/auth/useCases/verifyToken";
 import { searchController } from "../modules/friends/useCases/search";
+import { sendController } from "../modules/friends/useCases/send";
 
+export const friendsRoutes = Router();
 
-export const friendsRoutes = Router()
+friendsRoutes.get("/search", (res, req) => searchController.handle(res, req));
 
-friendsRoutes.get("/search", 
+friendsRoutes.post(
+  "/send",
   (res, req, next) => verifyTokenController.handle(res, req, next),
-  (res, req, next) => searchController.handle(res, req))
+  (res, req, next) => sendController.handle(res, req));
