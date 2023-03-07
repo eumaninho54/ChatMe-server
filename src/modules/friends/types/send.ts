a0aa2e413838d42f099a1e0e227b23bee517bb10
Angelo  Menti <angelo.omarzinho@outlook.com>
2023-03-06 23:49:31 -0300

    modules - types friends

---
+++ src/modules/friends/types/send.ts
@@ -0,0 +1,12 @@
+export type SendDTO = {
+  isAdded: boolean
+  idUser: string
+  idFriend: string
+  accessToken: string
+  refreshToken: string
+}
+
+export type BodyDTO = {
+  isAdded: boolean
+  idFriend: string
+}
