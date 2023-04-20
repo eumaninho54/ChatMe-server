d01358040d937c295eed50d062447b3ddce9de65
Angelo  Menti <angelo.omarzinho@outlook.com>
2023-04-19 23:20:56 -0300

    feat - init oneSignal pushNotification

--- src/websockets/index.ts
+++ src/websockets/index.ts
@@ -1,6 +1,9 @@
+import { CreateNotificationBody } from "onesignal-node/lib/types";
 import { prisma } from "../config/prismaClient";
 import { io } from "../http";
 import { IChatProps, IMessageProps } from "./types";
+import * as OneSignal from 'onesignal-node';  
+require("dotenv").config();
 
 io.on("connection", socket => {
   socket.on("chat", async(data: IChatProps) => {
@@ -10,6 +13,7 @@ io.on("connection", socket => {
   })
 
   socket.on("message", async(data: IMessageProps) => {
+    const client = new OneSignal.Client(process.env.ONESIGNAL_APP_ID, process.env.ONESIGNAL_API_KEY);
     const chat = await prisma.chat.findFirst({ where: { id: data.idChat } })  
 
     const message = await prisma.chatMessage.create({ data: {
@@ -18,6 +22,23 @@ io.on("connection", socket => {
       createdAt: new Date(),
       message: data.message
     }})
+
+    try {
+      const notification: CreateNotificationBody = {
+        contents: {
+          
+        },
+        include_player_ids: []
+      }
+
+      client.createNotification(notification)
+    } 
+    catch (err) {
+      if (err instanceof OneSignal.HTTPError) {
+        console.log(err.statusCode);
+        console.log(err.body);
+      }
+    }
 
     io.to(data.idChat).emit("message", {idChat: chat.id, message: message})
   })
