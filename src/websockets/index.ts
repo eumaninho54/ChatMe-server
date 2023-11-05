0f9f9d22506a84ac05b1c8c0a59f67fff671b18a
Angelo  Menti <angelo.omarzinho@outlook.com>
2023-11-05 01:58:01 -0300

    feat - webSocket onReceiveMessage

--- src/websockets/index.ts
+++ src/websockets/index.ts
@@ -122,6 +122,44 @@ io.on("connection", socket => {
     })
   })
 
+  socket.on("receiveMessage", async(data: IReceiveMessageProps) => {
+    const chat = await prisma.chat.findFirst({ where: { id: data.idChat } })
+
+    await prisma.chatMessage.update({
+      where: {
+        id: data.messageId 
+      },
+      data: {
+        isReceivedBy: {          
+          push: data.idUser
+        }
+      }
+    })
+
+    const allMessages = await prisma.chatMessage.findMany({ 
+      where: { idChat: data.idChat },
+      select: { 
+        id: true,
+        message: true,
+        isReceivedBy: true,
+        isReadBy: true,
+        createdAt: true,
+        sender: { 
+          select: { 
+            id: true,
+            name: true,
+            email: true,
+            isActive: true,
+          } 
+        },
+      },
+      orderBy: { createdAt: "desc" }
+    })
+
+    io.to(data.idChat).emit("editMessage", {
+      idChat: chat.id, 
+      messages: allMessages
+    })
   })
 })
 
